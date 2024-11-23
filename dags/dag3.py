6a6bcded039e50aa853645af95d7e837c0f3860c
Faishal Rayyan <faishalrayyan@Faishals-MacBook-Pro.local>
2024-11-23 18:18:21 +0700

    edited dags for black

--- dags/dag3.py
+++ dags/dag3.py
@@ -1,6 +1,7 @@
+from datetime import datetime
+
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
-from datetime import datetime
 
 DEFAULT_ARGS = {
     "owner": "faishalrayyan",
@@ -12,18 +13,18 @@ DEFAULT_ARGS = {
 
 # Define the DAG
 dag = DAG(
-    dag_id='daily_dummy_dag',  # DAG ID
-    description='A dummy DAG that runs once a day',
+    dag_id="daily_dummy_dag",  # DAG ID
+    description="A dummy DAG that runs once a day",
     default_args=DEFAULT_ARGS,
-    schedule_interval='@daily',  # Run once a day at midnight
+    schedule_interval="@daily",  # Run once a day at midnight
     start_date=datetime(2024, 1, 1),  # Start date for the DAG
     catchup=False,
-    tags=['dag3']
+    tags=["dag3"],
 )
 
 # Define the dummy task (no actual work is done)
 dummy_task = DummyOperator(
-    task_id='dummy_task',  # Task ID
+    task_id="dummy_task",  # Task ID
     dag=dag,  # The DAG the task belongs to
 )
 
